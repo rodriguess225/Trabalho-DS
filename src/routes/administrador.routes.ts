74bd0a2d4a5716a6e8fcb63e9311ff52be539d65
GuilhermeSilva06 <up202404271@up.pt>
2026-06-03 18:26:33 +0100

    app.ts atualizado

--- src/routes/administrador.routes.ts
+++ src/routes/administrador.routes.ts
@@ -6,5 +6,5 @@ const controller = new AdministradorController();
 
 routes.get('/', controller.listar.bind(controller));
 routes.post('/', controller.criar.bind(controller));
-
+routes.get('/:id', controller.buscarPorId.bind(controller));
 export default routes;
